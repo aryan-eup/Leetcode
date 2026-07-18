f0702a4fd783e888189b4e764dcd30cef33775db
Aryan patial <aryanpatial.cs@gmail.com>
2026-07-18 13:24:38 +0530

    Time: 0 ms (100%), Space: 11.4 MB (56.67%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int sum=0;
-        for(int i=0;i<accounts.size();i++){
-            int cursum=0;
-            for(int j=0;j<accounts[0].size();j++){
-                cursum+=accounts[i][j];
-            }
-            sum=max(sum,cursum);
+        for(const auto& val:accounts){
+            sum=max(sum,accumulate(val.begin(),val.end(),0));
         }
         return sum;
     }
