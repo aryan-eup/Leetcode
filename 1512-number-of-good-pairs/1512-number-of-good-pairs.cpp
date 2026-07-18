ae0a1e1c1761b0eb893140a411116316ac4d3f23
Aryan patial <aryanpatial.cs@gmail.com>
2026-07-18 16:49:41 +0530

    Time: 0 ms (100%), Space: 9.9 MB (23.78%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans+=mp[nums[i]]++;
+        }
+        return ans;
+    }
+};
