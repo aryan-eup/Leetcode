fb532cdf9df32471fa88e9142ed2e8ca5f258b17
Aryan patial <aryanpatial.cs@gmail.com>
2026-07-19 17:54:45 +0530

    Time: 2 ms (3.21%), Space: 8.6 MB (43.68%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        long long p=n;
+        if(p<0){
+            x=1/x;
+            p=-p;
+        }
+        while(p>0){
+            if(p%2==1){
+                ans*=x;
+            }
+            x=x*x;
+            p=p/2;
+        }
+        return ans;
+
+    }
+};
