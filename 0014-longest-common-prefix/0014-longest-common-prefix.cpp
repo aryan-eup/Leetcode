504da98cc3999638535b6169475af3ac46a9f1b0
Aryan patial <aryanpatial.cs@gmail.com>
2026-07-25 16:19:05 +0530

    Time: 0 ms (100%), Space: 11.6 MB (99.71%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        for(int i=0;i<strs[0].size();i++){
+            char ck=strs[0][i];
+            for(int j=1;j<strs.size();j++){
+                if(i==strs[j].size() || strs[j][i]!=ck){
+                    return strs[0].substr(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
