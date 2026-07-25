1ed9423fd498c5961ffc28df5294081152c275b4
Aryan patial <aryanpatial.cs@gmail.com>
2026-07-25 16:30:11 +0530

    Time: 0 ms (100%), Space: 11.6 MB (93.7%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<strs[0].size();i++){
             int ch=strs[0][i];
             for(int j=1;j<strs.size();j++){
-                if(i>strs[j].size()||strs[j][i]!=ch){
+                if(i==strs[j].size()||strs[j][i]!=ch){
                     return strs[0].substr(0,i);
                 }
             }
