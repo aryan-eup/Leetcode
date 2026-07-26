77c459cfaa20ba233dc448a99b852d42820700e4
Aryan patial <aryanpatial.cs@gmail.com>
2026-07-26 11:18:01 +0530

    Time: 0 ms (100%), Space: 31.6 MB (10.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        vector<int>neg;
-        vector<int>pos;
-        for(auto &x:nums){
-            x<0?neg.push_back(x):pos.push_back(x);
-        }
-        if(neg.size()>1){
-            sort(neg.begin(),neg.end(),greater<int>());
-        }
-        if(pos.size()>1){
-            sort(pos.begin(),pos.end());
-        }
-        int ng=neg.size();
-        int np=pos.size();
-        if(ng==0 || ng==1){
-            return pos[np-1]*pos[np-2]*pos[np-3];
-        }
-        if(pos.size()==0){
-            return neg[0]*neg[1]*neg[2];
-        }
-        if(neg.size()>=2){
-            if(pos.size()==1){
-                return neg[ng-1]*neg[ng-2]*pos[np-1];
-            }else{
-                if(neg[ng-1]*neg[ng-2]>pos[np-1]*pos[np-2]){
-                    return neg[ng-1]*neg[ng-2]*pos[np-1];
-                }
-                if(pos.size()>2){
-                    if(neg[ng-1]*neg[ng-2]*pos[np-1]>pos[np-1]*pos[np-2]*pos[np-3]){
-                        return neg[ng-1]*neg[ng-2]*pos[np-1];
-                    }
-                }
+        int m1=INT_MIN,m2=INT_MIN,m3=INT_MIN,s1=INT_MAX,s2=INT_MAX;
+        for(int x :nums){
+            if(x>=m1){
+                m3=m2;
+                m2=m1;
+                m1=x;
+            }else if(x>=m2){
+                m3=m2;
+                m2=x;
+            }else if(x>=m3){
+                m3=x;
+            }
+            if(x<=s1){
+                s2=s1;
+                s1=x;
+            }else if(x<=s2){
+                s2=x;
             }
         }
-        if(pos.size()==2){
-            return neg[ng-1]*neg[ng-2]*pos[np-1];
-        }
-        return pos[np-1]*pos[np-2]*pos[np-3];
+        return max((m1*m2*m3),(s1*s2*m1));
     }
 };
