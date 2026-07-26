f8d2a68d9b78496e2053f2bde9e848b5275b7d72
Aryan patial <aryanpatial.cs@gmail.com>
2026-07-26 21:25:50 +0530

    Time: 52 ms (81.08%), Space: 23.9 MB (86.88%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -12,8 +12,8 @@ public:
         ListNode* a=headA;
         ListNode* b=headB;
         while(a!=b){
-            a=(a==nullptr)?a=headB:a=a->next;
-            b=(b==nullptr)?b=headA:b=b->next;
+            a=(a==nullptr)?headB:a->next;
+            b=(b==nullptr)?headA:b->next;
         }
         return a;
     }
