a8999680cbea9f7da0e1c5d183d9a2add0c1122c
Aryan patial <aryanpatial.cs@gmail.com>
2026-07-30 19:40:09 +0530

    Time: 0 ms (100%), Space: 12.7 MB (63.06%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,19 +3,19 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int>mp;
         stack<int>st;
-        for(int num:nums2){
-            while(!st.empty() && num>st.top()){
-                mp[st.top()]=num;
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!st.empty() && nums2[i]>=st.top()){
                 st.pop();
             }
-            st.push(num);
-        }
-        while(!st.empty()){
-            mp[st.top()]=-1;
-            st.pop();
+            if(st.empty()){
+                mp[nums2[i]]=-1;
+            }else{
+                mp[nums2[i]]=st.top();
+            }
+            st.push(nums2[i]);
         }
         vector<int>ans;
-        for(auto it:nums1){
+        for(auto it :nums1){
             ans.push_back(mp[it]);
         }
         return ans;
