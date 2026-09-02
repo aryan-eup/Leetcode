0cbb7d8e7bf79aa5b3f23729a04d3a9176de2b5b
Aryan patial <aryanpatial.cs@gmail.com>
2026-09-02 22:42:09 +0530

    Time: 0 ms (100%), Space: 68.3 MB (27.42%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        if(n<1) return 0;
+        int ans=1;
+        int i=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                ans+=1;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                ans+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                ans+=down;
+                i++;
+                down++;
+            }
+            if(down>peak){
+                ans+=down-peak;
+            }
+        }
+        return ans;
+    }
+};
