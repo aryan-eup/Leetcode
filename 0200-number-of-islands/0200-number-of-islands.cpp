4620b6e537c66101c32b4ce911b6f047dc386aa4
Aryan patial <aryanpatial.cs@gmail.com>
2026-09-16 21:29:50 +0530

    Time: 59 ms (6.88%), Space: 24.5 MB (9.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void sear(vector<vector<char>>& grid , int r,int c){
-        int ro=grid.size();
-        int co=grid[0].size();
-        if(r<0 || r>=ro || c<0 || c>=co || grid[r][c]=='0'){
-            return;
+    void dfs(int row,int col,vector<vector<char>>& grid,int n,int m){
+        grid[row][col]=0;
+        vector<int>rd={0,+1,0,-1};
+        vector<int>cd={-1,0,+1,0};
+        for(int i=0;i<4;i++){
+            int nrow=row+rd[i];
+            int ncol=col+cd[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1'){
+                dfs(nrow,ncol,grid,n,m);
+            }
         }
-        grid[r][c]='0';
-        sear(grid,r,c-1);
-        sear(grid,r+1,c);
-        sear(grid,r,c+1);
-        sear(grid,r-1,c);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int r=grid.size();
-        int c=grid[0].size();
+        int n=grid.size();
+        int m=grid[0].size();
         int isl=0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
                     isl++;
-                    sear(grid,i,j);
+                    dfs(i,j,grid,n,m);
                 }
             }
         }
