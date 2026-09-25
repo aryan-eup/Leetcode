4b0a243088720a2d7cdcd9356e63d180749820b7
Aryan patial <aryanpatial.cs@gmail.com>
2026-09-25 12:28:51 +0530

    Time: 14 ms (33.43%), Space: 37.4 MB (31.99%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -16,6 +16,7 @@ public:
         while(!q.empty()){
             auto [dist,node]=q.top();
             q.pop();
+            if(dist>dis[node]) continue;
             for(auto it:adj[node]){
                 int nex=it.first;
                 long long edw=it.second;
